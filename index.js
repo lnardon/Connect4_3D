3d227a3c27cf354c37f43e509266137ca1792fe6
Lucas Nardon <lucaslnardon@gmail.com>
2021-11-02 21:39:10 -0300

    Add orbit controls and game board

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // IMPORTS
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js";
 
 //SCENE
 const scene = new THREE.Scene();
@@ -36,21 +35,49 @@ window.addEventListener(
 //LIGHTS
 const light1 = new THREE.AmbientLight(0xffffff, 0.5);
 const light2 = new THREE.PointLight(0xffffff, 1);
-
 scene.add(light1);
 scene.add(light2);
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
 
 //OBJECT
 const geometry = new THREE.CubeGeometry(100, 100, 100);
 const material = new THREE.MeshLambertMaterial({ color: 0xf3ffe2 });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// scene.add(mesh);
+
+// Helpers
+let squares = [];
+function createGame() {
+  requestAnimationFrame(render);
+  squares = [];
+  let x = 0;
+  let z = -2;
+  for (let i = 0; i < 25; i++) {
+    const geometry = new THREE.BoxGeometry(20, 5, 20);
+    const material = new THREE.MeshLambertMaterial({ color: 0xfafafa });
+    const square = new THREE.Mesh(geometry, material);
+    if (i % 5 === 0) {
+      x = -1;
+      z++;
+    }
+    square.rotateX(Math.PI / 1);
+    square.rotateY(Math.PI / 2);
+    square.position.set(23 * x, -10, 23 * z);
+    square.inUse = false;
+    squares.push(square);
+    scene.add(squares[i]);
+    x++;
+  }
+}
+createGame();
 
 //RENDER LOOP
-requestAnimationFrame(render);
 function render() {
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.03;
+  controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
